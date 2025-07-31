6e1384f148820cffba5eb7ee408d4e8bd0ff6a98
Wai Yan Phyo Oo <waiyanphyooo@protonmail.ch>
2025-07-31 19:13:38 +0630

    update

--- src/main/java/dao/java/UserDAO.java
+++ src/main/java/dao/java/UserDAO.java
@@ -44,7 +44,7 @@ public class UserDAO {
             return null;
         }
     }
-wai yan phyo
+wai yan p
     private static class UserRowMapper implements RowMapper<User> {
         @Override
         public User mapRow(ResultSet rs, int rowNum) throws SQLException {
